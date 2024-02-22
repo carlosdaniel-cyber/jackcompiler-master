71baca9de29f383cae0bcbeb639f92f76df6a502
Carlos Daniel <carlosdanielbp101@gmail.com>
2024-02-21 22:08:00 -0300

    Implementa geração de código dos arrays

--- src/main/java/br/ufma/ecp/Parser.java
+++ src/main/java/br/ufma/ecp/Parser.java
@@ -153,13 +153,20 @@ public class Parser {
                 if (peekTokenIs(TokenType.LPAREN) || peekTokenIs(TokenType.DOT)) {
                     parseSubroutineCall();
                 } else { 
-                    if (peekTokenIs(TokenType.LBRACKET)) { 
-                        expectPeek(TokenType.LBRACKET);
-                        parseExpression();                        
-                        expectPeek(TokenType.RBRACKET);                       
+                    if (peekTokenIs(LBRACKET)) { // array
+                        expectPeek(LBRACKET);
+                        parseExpression();
+                        vmWriter.writePush(kind2Segment(sym.kind()), sym.index());
+                        vmWriter.writeArithmetic(Command.ADD);
+        
+
+                        expectPeek(RBRACKET);
+                        vmWriter.writePop(Segment.POINTER, 1); // pop address pointer into pointer 1
+                        vmWriter.writePush(Segment.THAT, 0);   // push the value of the address pointer back onto stack
+        
                     } else {
                         vmWriter.writePush(kind2Segment(sym.kind()), sym.index());
-                    }
+                    }                
                 }
                 break;            
             case LPAREN:
@@ -254,24 +261,34 @@ public class Parser {
 
         var symbol = symbolTable.resolve(currentToken.lexeme);
 
-        if (peekTokenIs(TokenType.LBRACKET)) {
-            expectPeek(TokenType.LBRACKET);
-            parseExpression();         
-            expectPeek(TokenType.RBRACKET);
+        if (peekTokenIs(LBRACKET)) { // array
+            expectPeek(LBRACKET);
+            parseExpression();
+            
+            vmWriter.writePush(kind2Segment(symbol.kind()), symbol.index());
+            vmWriter.writeArithmetic(Command.ADD);
+    
+            expectPeek(RBRACKET);
+
+
 
             isArray = true;
         }
 
-        expectPeek(TokenType.EQ);
+        expectPeek(EQ);
         parseExpression();
 
         if (isArray) {
-    
+
+            vmWriter.writePop(Segment.TEMP, 0);    // push result back onto stack
+            vmWriter.writePop(Segment.POINTER, 1); // pop address pointer into pointer 1
+            vmWriter.writePush(Segment.TEMP, 0);   // push result back onto stack
+            vmWriter.writePop(Segment.THAT, 0);    // Store right hand side evaluation in THAT 0.
+
 
         } else {
             vmWriter.writePop(kind2Segment(symbol.kind()), symbol.index());
         }
-
 
         expectPeek(TokenType.SEMICOLON);
         printNonTerminal("/letStatement");
--- src/test/java/br/ufma/ecp/CodeGerenerator.java
+++ src/test/java/br/ufma/ecp/CodeGerenerator.java
@@ -338,4 +338,40 @@ public class CodeGerenerator extends TestSupport {
         assertEquals(expected, actual);
     }
 
+    @Test
+    public void arrayTest () {
+        var input = """
+            class Main {
+                function void main () {
+                    var Array v;
+                    let v[2] = v[3] + 42;
+                    return;
+                }
+            }
+            """;;
+        var parser = new Parser(input.getBytes(StandardCharsets.UTF_8));
+        parser.parse();
+        String actual = parser.VMOutput();
+        String expected = """
+            function Main.main 1
+            push constant 2
+            push local 0
+            add
+            push constant 3
+            push local 0
+            add
+            pop pointer 1
+            push that 0
+            push constant 42
+            add
+            pop temp 0
+            pop pointer 1
+            push temp 0
+            pop that 0
+            push constant 0
+            return        
+                """;
+        assertEquals(expected, actual);
+    }
+
 }
